2ddd6016227ae72e955ff6a8fb8d7ef1aee63af9
Bryce Dorn <brycedorn@gmail.com>
2015-11-03 13:34:11 -0600

    refactor to use string/fixnum classes, gets rid of handler

--- lib/zipfips.rb
+++ lib/zipfips.rb
@@ -1,2 +1,46 @@
-require 'zipfips/version'
-require 'zipfips/handler'
+require 'json'
+
+module ZipFips
+
+  def is_zip?
+    !!fips_hash[self.to_s] && !is_fips?
+  end
+
+  def is_fips?
+    !!zips_hash[self.to_s]
+  end
+
+  def zipfips
+    is_zip? ? to_fips : to_zip
+  end
+
+  def to_zip
+    zips_hash[self.to_s].to_i
+  end
+
+  def to_fips
+    fips_hash[self.to_s].to_i
+  end
+
+  private
+
+  def fips_hash
+    @@fips_hash ||= JSON.parse(fips_file.read)
+  end
+
+  def fips_file
+    File.open("#{File.dirname(__FILE__)}/zipfips/data/data.json", "r")
+  end
+
+  def zips_hash
+    fips_hash.invert
+  end
+end
+
+class Fixnum
+  include ::ZipFips
+end
+
+class Integer
+  include ::ZipFips
+end
