a31eae9fcd636c5cc0628e372293d4597785e60c
Bryce Dorn <brycedorn@gmail.com>
2015-11-22 22:55:22 -0600

    adds test for invalid zip

--- spec/zipfips_spec.rb
+++ spec/zipfips_spec.rb
@@ -4,6 +4,9 @@ describe ZipFips do
   zip = 12345
   fips = 36093
 
+  invalid_zip = -1
+  predicted_fips = 36749
+
   it "should return the proper FIPS code for a ZIP code" do
     expect(zip.to_fips).to eq(fips)
   end
@@ -17,8 +20,12 @@ describe ZipFips do
     expect(zip.zipfips).to eq(fips)
   end
 
-  it "should work for strings or numbers" do
+  it "should work for strings and numbers" do
     expect(zip.to_fips).to eq(fips)
     expect(zip.to_s.to_fips).to eq(fips)
+  end
+
+  it "should predict FIPS for ZIP code not in census data" do
+    expect(invalid_zip.zipfips).to eq(predicted_fips)
   end
 end
